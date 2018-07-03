62788c45c5b128c344f9234e0464943e5ba9129f
Hussein <tejanahmedhu@gmail.com>
2018-07-03 18:16:56 +0100

    added Navbar variable to app.js

--- spring-and-react-boot-ams/src/main/resources/static/js/app.js
+++ spring-and-react-boot-ams/src/main/resources/static/js/app.js
@@ -10,6 +10,61 @@
     //     }
     // });
 
+    var Navbar = React.createClass({
+
+        render: function() {
+            return(
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a className="navbar-brand" href="/">AMS</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/dashboard">Dashboard <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/createAccount">Create Account <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/viewAccount">View Account <span className="sr-only">(current)</span></a>
+                            </li>
+                            {/* <li className="nav-item">
+             <a className="nav-link" href="/">Link</a>
+           </li> */}
+                            <li className="nav-item dropdown ">
+                                <a className="nav-link dropdown-toggle" onClick={this.showDropdownMenu} id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Account <span className="sr-only">(current)</span>
+                                </a>
+                                {this.state.displayMenu ? (
+                                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <a className="dropdown-item" href="/viewAccount">View Accounts</a>
+                                            <div className="dropdown-divider"></div>
+                                            <a className="dropdown-item" href="/createAccount">Add Account</a>
+                                            {/* <div className="dropdown-divider"></div>
+               <a className="dropdown-item" href="/">Something else here</a> */}
+                                        </div>
+                                    ):
+                                    (
+                                        null
+                                    )
+                                }
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </nav>
+            )
+        }
+
+
+    });
+
     var User = React.createClass({
     getInitialState: function() {
     return {display: true };
